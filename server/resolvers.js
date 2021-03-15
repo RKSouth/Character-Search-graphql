103dc5c8b825a0973abaf3a61e109fb4dbeeb712
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2021-03-15 13:43:34 -0700

    attacks query working

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,6 +1,8 @@
 const db = require('./db')
 
 const Query = {
+    attack: (root, {id}) => db.attacks.get(id),
+    attacks: () => db.attacks.list,
     arena: (root, {id}) => db.arenas.get(id),
     //our parent value is our root object, args contains the arguments passed in the graphql query
     // then we need to return the character
@@ -16,13 +18,14 @@ const Arena = {
 // we need to declare a new object (like the schema) of character where we can put functions to resolve the arena field
 const Character = {
     // the arena comes for the parent (character) and we want to get the arena with same id as the one listed with the character
-    arena: (character) => db.arenas.get(character.arenaId)
+    arena: (character) => db.arenas.get(character.arenaId),
+    attack: (character) => db.attacks.list()
+        .filter((attack) => attack.characterId === character.id)
 }
 
-const Attacks = {
-    attacks: (character) => db.character.list()
-        .filter((attacks) => attacks.characterId === character.id)
+const Attack = {
+    character: (attack) => db.characters.get(attack.characterId)
 }
 
 // make sure all the objects are being exported here
-module.exports = { Query, Arena, Character,Attacks }
+module.exports = { Query, Arena, Character, Attack }
